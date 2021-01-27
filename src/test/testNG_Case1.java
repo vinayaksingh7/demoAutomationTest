ae61b689fb36209532cabacd0115fccb872ec0d1
vinayaksingh7 <vinayaksingh2009@gmail.com>
2021-01-28 01:18:07 +0530

    First Commit

---
+++ src/test/testNG_Case1.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class testNG_Case1 {
+
+	@Test
+	public void Demo()
+	{
+		System.out.println("Hello");
+	}
+
+	@Test
+	public void secondTest()
+	{
+		System.out.println("Bye");
+	}
+
+	
+}
---
+++ src/test/testNG_xmlTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class testNG_xmlTest {
+
+	@Test
+	public void Demo()
+	{
+		System.out.println("Testing the xml");
+	}
+}
